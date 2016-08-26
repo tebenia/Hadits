32d533135ffad1a210b95f6a5a846297b18bf007
tebenia <tebe.deye@gmail.com>
2016-08-26 10:42:10 +0700

    12. Add a Simple Hadits Schema

---
+++ imports/api/hadits/hadits.jsx
@@ -0,0 +1,40 @@
+import {Mongo} from 'meteor/mongo';
+
+export const Hadits = new Mongo.Collection("hadits");
+Hadits.allow({
+  insert: function(){
+    return false;
+  },
+  update: function(){
+    return false;
+  },
+  remove: function(){
+    return false;
+  }
+});
+
+Hadits.deny({
+  insert: function(){
+    return true;
+  },
+  update: function(){
+    return true;
+  },
+  remove: function(){
+    return true;
+  }
+});
+
+const HaditsSchema = new SimpleSchema({
+  title:{
+    type: String,
+    label: "Title"
+  },
+  createdAt: {
+    type: Date,
+    label: "Date submitted",
+    autoValue(){
+      return new Date();
+    }
+  }
+});
